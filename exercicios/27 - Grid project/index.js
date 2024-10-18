e51d9b26a89701e39a08ddf9fb182fb45197457f
matheusoliveira0 <teumatheus.73@gmail.com>
2024-10-18 17:57:14 -0300

    Colt2

---
+++ exercicios/27 - Grid project/index.js
@@ -0,0 +1,12 @@
+const hamburguer = document.querySelector(".hamburguer");
+const bar1= document.querySelector(".bar1");
+const bar2= document.querySelector(".bar2");
+const bar3= document.querySelector(".bar3");
+const mobileNav = document.querySelector(".mobileNav");
+
+hamburguer.addEventListener("click", () => {
+    bar1.classList.toggle("animateBar1");
+    bar2.classList.toggle("animateBar2");
+    bar3.classList.toggle("animateBar3");
+    mobileNav.classList.toggle("openDrawer")
+});
